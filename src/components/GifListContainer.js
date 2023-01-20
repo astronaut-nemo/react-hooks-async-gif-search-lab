807d17eeeedaa9905371d1a2f7b7ebf5889e0cdb
Joanna <astronautnemo@gmail.com>
2023-01-21 00:43:06 +0300

    removed URI Encoding

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -8,8 +8,10 @@ function GifListContainer() {
     const [search ,setSearch] = useState("dogs");
 
     // Fetch Requests
+    // Based on the search state, fetch gifs from the API and set the gifs state to the data key of the response. 
+    // URI is encoded to allow multiple word searches
     useEffect(() =>{
-        fetch(encodeURI(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=XWBNd1ksECRTja7rylogKwDat6SXW8Pi&limit=3&rating=g`))
+        fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=XWBNd1ksECRTja7rylogKwDat6SXW8Pi&limit=3&rating=g`)
         .then((response) => response.json())
         .then((fetchedGifs) => setGifs(fetchedGifs.data));
     }
@@ -17,7 +19,6 @@ function GifListContainer() {
     );
 
     // Event Handlers
-    // Get the search term from GifSearch and encode it, then pass it to the fetch as a dependancy
     function handleSearchSubmit(searchTerm){
         setSearch(searchTerm);
     }
